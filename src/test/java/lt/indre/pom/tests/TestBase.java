04300e4ad8bf4e90c578ef4b7af2ac4a4e175e05
Indre_Pauksta <pauksta.indre@gmail.com>
2023-11-12 19:45:25 +0200

    Create BeforeMethod and AfterMethod

--- src/test/java/lt/indre/pom/tests/TestBase.java
+++ src/test/java/lt/indre/pom/tests/TestBase.java
@@ -1,4 +1,20 @@
 package lt.indre.pom.tests;
 
-public class TestBase {
+import lt.indre.pom.pages.Common;
+import lt.indre.pom.utilities.TestListener;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+
+@Listeners(TestListener.class)
+public abstract class TestBase {
+    @BeforeMethod
+    public abstract void setUp();
+
+
+    @AfterMethod
+    public void tearDown() {
+        Common.quitDriver();
+    }
+
 }
