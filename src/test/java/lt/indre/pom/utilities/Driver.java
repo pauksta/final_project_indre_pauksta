fbcb85bbb956aa4be60ff983655822858606ed0d
Indre_Pauksta <pauksta.indre@gmail.com>
2023-11-12 19:31:45 +0200

    Chromedriver setup in Driver Class

--- src/test/java/lt/indre/pom/utilities/Driver.java
+++ src/test/java/lt/indre/pom/utilities/Driver.java
@@ -1,6 +1,35 @@
 package lt.indre.pom.utilities;
 
-public class Driver {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
+public class Driver {
+    private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+
+    public static void setChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--force-device-scale-factor=0.75");
+        options.addArguments("--start-maximized");
+//        options.addArguments("--headless=new");
+
+        drivers.set(new ChromeDriver(options));
+        drivers.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+    }
+
+    public static WebDriver getDriver () {
+        return drivers.get();
+    }
+
+    public static void quitDriver () {
+//        drivers.get().quit();
+//        drivers.remove();
+    }
 
 }
